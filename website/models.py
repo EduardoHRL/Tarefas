632d619adcca97ecbcb818e3d1a14d9258f3e082
EduardoHRL <eduardohrl18@gmail.com>
2024-11-21 21:04:22 -0300

    Reapply "A"
    This reverts commit b7ae4c9321f8f0556ed4a180ff415197501c4060.

--- website/models.py
+++ website/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-
-class Usuario(models.Model):
-    usu_codigo = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255, db_column='usu_nome')
-    email = models.EmailField(max_length=255, db_column='usu_email')
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        db_table = 'tbl_usuarios'
-
-
+from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
     tar_codigo = models.AutoField(primary_key=True)
@@ -20,9 +8,10 @@ class Tarefa(models.Model):
     prioridade = models.CharField(max_length=20, db_column='tar_prioridade')
     status = models.CharField(max_length=20, db_column='tar_status')
     data = models.DateField(db_column='tar_data')
-    usuario = models.ForeignKey(
-        Usuario, on_delete=models.CASCADE, related_name='tarefas', db_column='usu_codigo'
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descricao
 
     class Meta:
         db_table = 'tbl_tarefas'
